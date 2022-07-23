874cba9365119fcd89db5095776958d1dfdb7456
munjeongmin <3330kys@dsm.hs.kr>
2022-07-23 09:54:08 +0900

    first commit

---
+++ src/main/java/com/example/dockerkubernetespr/DockerKubernetesPrApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerkubernetespr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerKubernetesPrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerKubernetesPrApplication.class, args);
+    }
+
+}
